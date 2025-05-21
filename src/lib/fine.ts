047e22c76c68d561175652643fe89e0f8f8d9061
Yotam Haran <yotam@fine.dev>
2025-05-21 14:17:51 +0300

    test 123

--- src/lib/fine.ts
+++ src/lib/fine.ts
@@ -3,4 +3,4 @@ import { FineClient } from "@fine-dev/fine-js";
 /**
  * This variable will hold the Fine client. It will be populated automatically by Fine when you have a backend set up
  */
-export const fine: FineClient | null = null;
+export const fine: FineClient = null as unknown as FineClient;
