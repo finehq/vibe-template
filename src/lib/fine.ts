4c0381b587aba2a23c6ea3df09460c1e8a26e87e
Yotam Haran <yotam@fine.dev>
2025-05-26 12:31:12 +0300

    bugfix + cursor rules fixed

--- src/lib/fine.ts
+++ src/lib/fine.ts
@@ -1,4 +1,4 @@
 import { FineClient } from "@fine-dev/fine-js";
 import type { Schema } from "./db-types";
 
-export const fine = new FineClient<Schema>("/api");
+export const fine = new FineClient<Schema>("/api/fine");
